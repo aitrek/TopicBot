a620a053b44d77333fda03445fa51e5677aea3f3
aitrek <aitrek.zh@gmail.com>
2018-09-23 00:14:05 +0800

    update Context

--- topicbot/context.py
+++ topicbot/context.py
@@ -5,15 +5,20 @@ import json
 
 class Context:
 
-    def __init__(self):
-        pass
-
-    def __repr__(self) -> str:
-        return json.dumps(self.values)
+    def __init__(self, msg: dict):
+        self._user_id = str(msg["user_id"]).strip()
+        self._text = msg["text"].strip()
+        self._context_data = self._create_context(msg)
 
     @property
     def values(self) -> dict:
         return self.__dict__
+
+    def __repr__(self) -> str:
+        return json.dumps(self.values)
+
+    def _create_context(self, msg: dict):
+        raise NotImplementedError
 
     def to_features(self) -> dict:
         """Extract extended features from the context data"""
