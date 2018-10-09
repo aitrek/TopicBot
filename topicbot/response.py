cfb2519bbbd26244c42d134d101d5338e161a8f1
aitrek <aitrek.zh@gmail.com>
2018-10-10 00:09:16 +0800

    add export method to Response

--- topicbot/response.py
+++ topicbot/response.py
@@ -1,6 +1,18 @@
 """Class to handle the output"""
 
+from typing import Tuple, List, Union
+
 
 class Response:
 
-    pass
+    def _export(self) -> Union[dict, List[dict], Tuple[dict]]:
+        raise NotImplementedError
+
+    def export(self) -> List[dict]:
+        output = self._export()
+        if isinstance(output, dict):
+            return [output]
+        elif isinstance(output, (list, tuple)):
+            return output
+        else:
+            return []   # todo
