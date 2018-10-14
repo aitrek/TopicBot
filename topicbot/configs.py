9ba3a6c991d9ddfcd92b1e1ccbf4c82b280623b6
aitrek <aitrek.zh@gmail.com>
2018-10-14 13:48:35 +0800

    update Configs to avoid repeatedly reading

--- topicbot/configs.py
+++ topicbot/configs.py
@@ -11,15 +11,16 @@ class Configs:
 
     def __init__(self):
         self._parser = ConfigParser()
-        self._loaded = False
+        self._has_loaded = False
 
     def get(self, section: str, option: str) -> str:
-        if not self._loaded:
+        if not self._has_loaded:
             raise ConfigDataError
         return self._parser.get(section, option)
 
     def read(self, filenames, encoding=None):
         """Read and parse the configs file"""
-        self._parser.read(filenames, encoding=encoding)
-        self._loaded = True
+        if not self._loaded:
+            self._parser.read(filenames, encoding=encoding)
+            self._has_loaded = True
         return self
