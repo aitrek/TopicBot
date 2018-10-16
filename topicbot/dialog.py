9da765d5bfa1520eb52b369f6960670286279551
aitrek <aitrek.zh@gmail.com>
2018-10-16 15:05:39 +0800

    finish the main structure of Dialog

--- topicbot/dialog.py
+++ topicbot/dialog.py
@@ -3,8 +3,55 @@
 import logging
 
 from .base import Base
+from .response import Response
 
 
 class Dialog(Base):
 
-    pass
+    _attrs = [
+        "_id",              # str, instance identifier
+        "_msg",             # dict, client msg
+        "_parsed_data",     # dict, parsed data of msg
+        "_domain",          # str, domain of the conversation
+        "_intent",          # str, intent of the domain
+        "_cases",           # List[str], cases of the intent
+        "_response"         # instance, final response to user
+    ]
+
+    def __init__(self, msg: dict, context: dict, grounding: dict):
+        super().__init__()
+        self._msg = msg
+        self._context = context
+        self._grounding = grounding
+        self._domain = None
+        self._intent = None
+        self._cases = None
+        self._response = None
+        self._parse()
+
+    @property
+    def domain(self):
+        return self._domain
+
+    @property
+    def intent(self):
+        return self._intent
+
+    @property
+    def cases(self):
+        return self._cases
+
+    @property
+    def response(self):
+        return self._response
+
+    @response.setter
+    def response(self, response: Response):
+        self._response = response
+
+    @property
+    def name(self):
+        return self.domain + "." + self.intent if self.intent else self.domain
+
+    def _parse(self):
+        raise NotImplementedError
