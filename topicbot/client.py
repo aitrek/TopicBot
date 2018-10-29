24791df3fe8ec75f77712d9d2597e9232326a255
aitrek <aitrek.zh@gmail.com>
2018-10-29 11:08:43 +0800

    add method in Client to extract context data

--- topicbot/client.py
+++ topicbot/client.py
@@ -162,11 +162,16 @@ class Client(Base):
         if self._topic is not None:
             self._previous_topics[self._topic.id] = self._topic.status()
 
+    def _extract_context_from_msg(self, msg: dict) -> dict:
+        """Extract necessary data from msg to initialize a Context instance."""
+        raise NotImplementedError
+
     def _update(self, msg: dict):
         """
         Update client data(dialog, context, grounding, previous_topics)
         with input message from user.
         """
+        self._context = self._class_context(self._extract_context_from_msg(msg))
         self._dialog = self._class_dialog(msg, self.context, self.grounding)
         self._topic = self._create_topic()
         self._context.update(self._dialog)
